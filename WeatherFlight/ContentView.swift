1c6ee04139834fb2b5b517b4861b1a1295400191
M-Alcantar <alcantar.mauricio@cetys.edu.mx>
2025-05-14 18:35:46 -0700

    I bub wid it

--- WeatherFlight/ContentView.swift
+++ WeatherFlight/ContentView.swift
@@ -1,12 +1,4 @@
-//
-//  ContentView.swift
-//  WeatherFlight
-//
-//  Created by David Emmanuel Santana Romero on 12/05/25.
-//
-
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -16,70 +8,28 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
 
+
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        TabView {
+            SearchView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            SearchView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+                .tabItem {
+                    Image(systemName: "square.grid.2x2.fill")
+                    Text("Category")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+            SearchView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+                .tabItem {
+                    Image(systemName: "plus.circle.fill")
+                    Text("Add")
                 }
-            }
-            Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        .accentColor(.purple)
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
