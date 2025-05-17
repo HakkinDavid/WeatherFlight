b5f31e1bd6794a744d436b0574a1659c155bb19a
HakkinDavid <desr11@hotmail.com>
2025-05-17 01:31:01 -0700

    plist

---
+++ WeatherFlight/AgendaView.swift
@@ -0,0 +1,18 @@
+//
+//  AgendaView.swift
+//  WeatherFlight
+//
+//  Created by David Emmanuel Santana Romero on 14/05/25.
+//
+
+import SwiftUI
+
+struct AgendaView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    AgendaView()
+}
--- WeatherFlight/WeatherFlightApp.swift
+++ WeatherFlight/WeatherFlightApp.swift
@@ -13,8 +13,7 @@ struct WeatherFlightApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            BeginScreen()
         }
     }
 }
