540b2a6df6fe1acab5932281037f0180647e9336
HakkinDavid <desr11@hotmail.com>
2025-05-19 20:41:27 -0700

    Create DateRangeModel.swift

---
+++ WeatherFlight/Models/DateRangeModel.swift
@@ -0,0 +1,37 @@
+//
+//  DateRangeModel.swift
+//  WeatherFlight
+//
+//  Created by David Emmanuel Santana Romero on 19/05/25.
+//
+
+import SwiftUI
+
+struct DateRange: Identifiable {
+    let id: UUID
+    let startDate: Date
+    let endDate: Date
+    let associate_id: UUID? = nil
+    
+    init(startDate: Date, endDate: Date) {
+        self.id = UUID()
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+    
+    func get_associate_id() -> UUID? {
+        return self.associate_id
+    }
+    
+    func get_start_date() -> Date {
+        return self.startDate
+    }
+    
+    func get_end_date() -> Date {
+        return self.endDate
+    }
+    
+    func get_duration() -> TimeInterval {
+        return self.endDate.timeIntervalSince(self.startDate)
+    }
+}
